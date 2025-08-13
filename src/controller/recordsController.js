4b75e162bf7967f5cf8d833c19a18c21eb9cbaac
juno <juno@jun-oui-noteubug.local>
2025-08-13 14:00:34 +0900

    recordsControllerrank&&UniqueRecord

--- src/controller/recordsController.js
+++ src/controller/recordsController.js
@@ -1,13 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import { orderBy } from "lodash";
-
+import { toDate } from "date-fns";
+import { type } from "node:os";
 const prisma = new PrismaClient();
-
+const allowedRanks = ["월간순","주간순"] 
 class RecordsController {
   createRecord(req, res, next){
   }
 
   async getRecordList(req, res, next){
+    
+    const {nickname} =req.query;
+
     // pagenation
     const {page, take, sort} = req.query;
 
@@ -20,17 +24,16 @@ class RecordsController {
     // 가지치기
     if(skip < 0 ) return res.status(400).json({error: "skip은 음수가 되면 안됩니다"})
     
-    if(!sortType.include(sort)) return res.status(400).json({error: "정렬 기능 범위 확인 "});
+    if(!sortType.includes(sort)) return res.status(400).json({error: "정렬 기능 범위 확인 "});
 
     try {
       const recordList = await prisma.record.findMany({
-        where:{
-          page:{pageNumber},
-          take:{takeNumber},
-          skip
-        },
+        where : nickname ? {nickname:{contains: nickname, mode:"insensitive" } }: {}, // nickname 으로 조회 가능
+        page: pageNumber ,
+        take: takeNumber ,
+        skip,
 
-        select:{ // 닉네임, 거리, 운동시간, 운동 종류, 사진 표시
+        select: { // 닉네임, 거리, 운동시간, 운동 종류, 사진 표시
           nickname : true,
           distance : true,
           activityType: true,
@@ -50,26 +53,100 @@ class RecordsController {
       })
     } catch (error) {
       console.error(error);
+      res.status(500).json(error.message);
+    }
+  }
+
+
+  getWeekDateRange(year, month, weekOfMonth) {
+    const firstOfmonth  = new Date(year,month  - 1, 1);
+    const startDate = new Date(firstOfmonth)
+    startDate.setDate (1 +(  weekOfMonth  - 1)* 7)
+    const endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + 6);
+
+    return { startDate, endDate };
+  }
+  async  getRankRecord(req, res, next){
+   
+    // pagination
+    const {page, take, sort: rank_type} = req.query;
+    const pageNumber = Number(page) || 1;
+    const takeNumber = Number(take) || 10;
+    const skip = (pageNumber - 1) * takeNumber
+    
+    // prunning 
+    if (isNaN(pageNumber) || isNaN(takeNumber)) return res.status(400).json({error: "페이지네이션 요청 리퀘스트 오류"})
+    if (skip < 0) return res.status(400).json({error : "페이지네이션 스킵값오류"})
+    if (!allowedRanks.includes(sort)) return res.status(400).json({error : "랭크 정렬 오류"})
+    try {
+      const now = new Date();
+      let  dateFilter = {};
+      if (rank_type === "월간순"){
+        const oneMonthAgo =  new Date(now)
+        oneMonthAgo.setMonth(now.getMonth() - 1)
+        dateFilter = {recordDate :{gte: oneMonthAgo, lte : now}}
+
+      }else if(rank_type === "주간순"){
+        const oneWeekAgo = new Date(now);
+        oneWeekAgo.setWeek(now.getWeek() - 7)
+        dateFilter = { recordDate : {gte : oneWeekAgo, lte : now}}
+      }
+      const rankList = await prisma.record.findMany({
+        page: pageNumber,
+        take: takeNumber,
+        skip,
+        where:{
+          duration: rank_type
+        },
+        orderBy:{
+          recordCount: "desc",
+        }, 
+        select: {
+          recordTime: true,
+          nickname: true,
+        }
+      });
+      res.status(200).json({
+        message : "랭크 리스트 조회 성공",
+        data : rankList
+      });
+      
+    } catch (error) {
+      console.error(error);
       res.status(500).json(error.message)
     }
   }
-  getRankRecord(req, res, next){
-   
+
+  async getRecord(req, res, next){
+    const groupId = req.params.groupId
+    const { description, activityType, distance, nickname, recordTime, photos } = req.query;
     
-    // prunning 
-   
+    const distanceNumber = Number(distance);
+    const recordTimeNumber = Number(recordTime);
+
+    if(typeof activityType !== "string" || typeof nickname !== "string" ) return res.status(400).json(error.message);
+
+    if(isNaN(groupId) || isNan(distanceNumber) || isNan(recordTimeNumber))return res.status(400).json(error.messsage);
     
     try {
-      const rank = await.prisma.record.findMany({})
-    } catch (error) {
-      
-    }
-  }
-
-  getRecord(req, res, next){
-
-    try {
-      
+      const uniqueRecord = await prisma.record.findUnique({
+        where:{
+          id: {groupId},
+        },
+        select:{
+          activityType: true,
+          distance: true,
+          recordTime : true,
+          nickname: true,
+          photos: true,
+          description: true
+        }
+      });
+      res.status(200).json({
+        message:"해당 그룹의 운동기록 조회 성공",
+        data : uniqueRecord
+      });
     } catch (error) {
       
     }

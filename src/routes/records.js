05f6554db762d311a0363e6a788f5c764c717dd8
juno <juno@jun-oui-noteubug.local>
2025-08-13 14:40:07 +0900

    route

--- src/routes/records.js
+++ src/routes/records.js
@@ -1,5 +1,11 @@
 import express from "express";
+import recordsController from "../controller/recordsController.js";
 
 const router = express.Router();
+
+const recordsController = new recordsController();
+router.get('/',recordsController.getRecordList)
+router.get('/',recordsController.getRankRecord)
+router.get('/:id', recordsController.getRecord)
 
 export default router;

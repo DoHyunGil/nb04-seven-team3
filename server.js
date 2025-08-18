f0d00aa6f51fa21a0f3aa8b7f22235909dd6fdd3
jhhong0571 <105486482+jhhong0571@users.noreply.github.com>
2025-08-18 17:37:08 +0900

    server.js #record.js 라우터와 연결

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import groupRouters from "./src/routes/groups";
+import recordsRouter from "./src/routes/records.js";
 
 const app = express();
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 app.use("/groups", groupRouters);
+app.use("/groups", recordsRouter);
 
 app.listen(PORT, () => {
   console.log("server running");

e9bee9375aebc792c07c40674f296fadbc215f58
raincode02 <coldheart2523@gmail.com>
2025-08-19 17:04:21 +0900

    [feat] 그룹관련 스키마 변경 사항 및 미완성본

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import express from "express";
-import groupRouters from "./src/routes/groups.js";
+import express from 'express';
+import groupRouters from './src/routes/groups.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
-app.use("/groups", groupRouters);
+app.use('/groups', groupRouters);
 
 app.listen(PORT, () => {
-  console.log("server running");
+  console.log('server running');
 });
--- src/controller/groupsController.js
+++ src/controller/groupsController.js
@@ -1,163 +1,129 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import { group } from 'console';
 
 const prisma = new PrismaClient();
 
-class GroupsContriller {
-  /**
-   *  시스템명 : SEVEN
-   *  API명 : 그룹 등록
-   *  @param : {*} RequestBody
-  */
-  createGroupRecord = async (req, res) => {
-    console.log('groupsController createGroupRecord()..');
+class GroupsController {
+  //그룹 목록 조회
+  async getAllGroups(req, res) {
+    try {
+      const {
+        page = 1,
+        limit = 10,
+        order = 'desc',
+        orderBy = 'createdAt',
+        search = '',
+      } = req.query;
 
-    try{
-
-        const { 
-          name,
-          description,
-          photoUrl,
-          goalRep,
-          likeCount = 0,
-          badgeYn = false,
-          point = 0,
-          discordWebhookUrl,
-          discordInviteUrl,
-          nickname,
-          password
-        } = req.body;
-
-        //필수값 검증
-        if(
-          !name ||
-          !description ||
-          !photoUrl ||
-          !goalRep ||
-          !discordWebhookUrl ||
-          !discordInviteUrl ||
-          !nickname ||
-          !password
-        ) {
-          return res.status(400).json({ error: "필수 작성 내용이 누락되었습니다."  })
-        };
-
-        //nickname 중복방지 체크
-        const dupNickname = await prisma.group.findMany({
-          where: {
-            nickname: nickname
+      const pageNum = Number(page);
+      const limitNum = Number(limit);
+      const where = search
+        ? {
+            name: {
+              contains: search,
+              mode: 'insensitive',
+            },
+          }
+        : {};
+      const groups = await prisma.group.findMany({
+        where,
+        skip: (pageNum - 1) * limitNum,
+        take: limitNum,
+        orderBy:
+          orderBy === 'participantCount'
+            ? { groupParticipants: { _count: order } }
+            : { [orderBy]: order },
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          photoUrl: true,
+          goalRep: true,
+          discordWebhookUrl: true,
+          discordInviteUrl: true,
+          recommendCount: true,
+          createdAt: true,
+          updatedAt: true,
+          badgeYn: true,
+          owner: {
+            select: {
+              id: true,
+              nickname: true,
+              createdAt: true,
+              updatedAt: true,
+            },
           },
-          select: { id: true, nickname: true, password: true },
-        });
-        if( isNaN(dupNickname) ) {
-          return res.status(400).json({ error: "중복된 닉네임이 존재합니다."  })
-        };
+          tags: {
+            select: {
+              tag: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+          groupParticipants: {
+            select: {
+              participant: {
+                select: {
+                  id: true,
+                  nickname: true,
+                  createdAt: true,
+                  undatedAt: true,
+                },
+              },
+            },
+          },
+        },
+      });
+      res.json(groups);
+    } catch (error) {
+      res.status(500).json({ error: `서버 오류 ${error.message}` });
+    }
+  }
 
-        const group = await prisma.group.create({
-              data: {
-                  name,
-                  description,
-                  photoUrl,
-                  goalRep,
-                  likeCount,
-                  badgeYn,
-                  point,
-                  discordWebhookUrl,
-                  discordInviteUrl,
-                  nickname,
-                  password
-              }
-        });
-        res.status(200).send(group);
-            
-      } catch(error) {
-          console.log(error);
-          res.status(400).json( { error: '그룹등록에 실패했습니다!'} );
+  // 그룹 상세 조회
+  async getGroupById(req, res) {
+    try {
+      const groupId = Number(req.params.groupId);
+      if (isNaN(groupId)) {
+        return res.status(400).json({ error: 'groupId가 유효하지 않습니다.' });
       }
-  };
+      const group = await prisma.group.findUnique({
+        where: { id: groupId },
+        select: {
+          name: true,
+          owner: {
+            select: {
+              nickname: true,
+            },
+          },
+          description: true,
+          photoUrl: true,
+          tags: {
+            select: {
+              tag: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+          goalRep: true,
+          _count: { select: { groupParticipants: true } },
+          discordInviteUrl: true,
+        },
+      });
+      const { _count, ...rest } = group;
 
-   /**
-   *  시스템명 : SEVEN
-   *  API명 : 그룹 수정
-   *  @param : {*} RequestBody
-  */
-   updateGroupRecord = async (req, res) => {
-    try{
-          const id = parseInt(req.params.id);
-          const { 
-              name,
-              description,
-              photoUrl,
-              goalRep,
-              likeCount = 0,
-              badgeYn = false,
-              point = 0,
-              discordWebhookUrl,
-              discordInviteUrl,
-              nickname,
-              password
-          } = req.body;
-
-          //필수값 검증
-          if(
-            !name ||
-            !description ||
-            !photoUrl ||
-            !goalRep ||
-            !discordWebhookUrl ||
-            !discordInviteUrl ||
-            !nickname ||
-            !password
-          ) {
-            return res.status(400).json({ error: "필수 작성 내용이 누락되었습니다."  })
-          };
-          
-          console.log(`groupsController updateGroupRecord()..  groupId:${id} `);
-
-          const group = await prisma.group.update({
-            where: { id },  
-            data: { 
-                  name,
-                  description,
-                  photoUrl,
-                  goalRep,
-                  likeCount,
-                  badgeYn,
-                  point,
-                  discordWebhookUrl,
-                  discordInviteUrl,
-                  nickname,
-                  password
-              }
-          });
-          res.status(200).send(group);
-            
-        } catch(error) {
-            console.log(error);
-            res.status(400).json( { error: '그룹수정에 실패했습니다!'} );
-        }
-   };
-
-   /**
-   *  시스템명 : SEVEN
-   *  API명 : 그룹 삭제
-   *  @param : {*} RequestBody
-   */
-   deleteGroupRecord = async (req, res) => {
-    try{
-          const id = parseInt(req.params.id);
-          console.log(`groupsController deleteGroupRecord()..  groupId:${id} `);
-
-          const group = await prisma.group.delete({
-            where: { id }  
-          });
-          res.status(200).send(group);
-            
-        } catch(error) {
-            console.log(error);
-            res.status(400).json( { error: '그룹삭제에 실패했습니다!'} );
-        }
-   };
-
+      res.status(200).json({
+        ...rest,
+        participantCount: _count.groupParticipants,
+      });
+    } catch (error) {
+      res.status(500).json({ error: `서버 오류 ${error.message}` });
+    }
+  }
 }
 
-export default new GroupsContriller();
+export default new GroupsController();

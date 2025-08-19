dfdef0a74cebeef6581028e543c0de2d5509410b
JaeYun Byun <bjy001@naver.com>
2025-08-19 14:47:09 +0900

    : second update

--- src/controller/recordsController.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-class RecordsContriller {
-  createRecord(req, res, next) {}
-}
-
-export default new RecordsContriller();

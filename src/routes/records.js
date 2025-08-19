a6520fe3a4b4ee5bfa3ef435b8cf5a518071a15f
jhhong0571 <105486482+jhhong0571@users.noreply.github.com>
2025-08-19 17:21:28 +0900

    merge: #운동 기록 생성 API (POST) (#18)
    * #운동 기록 생성 API (POST)
    
    1. POST -- 그룹 운동 기록 생성
    2. 검증 -- groupId 필수로 정수 체크
    3. 검증 -- #필수 작성 내용 누락 확인
    4. 검증 -- #ActivityType 매핑, 프론트엔드 인터페이스에 표기된대로
    5. 검증 -- 그룹 존재 유무 판단
    6. 검증 -- Participant (유저) 인증, 닉네임 조회, 비밀번호 확인
    7. 생성 -- 그룹 운동 기록 생성, 사진
    8. 응답 -- Response Body - 201 CREATED
    
    * records.js #운동 기록 생성- POST API - Express 라우팅
    
    Express 라우팅을 제외한 코드 별도로 분해
    
    * #운동기록 생성 API - Prisma, 로직
    
    Express 라우팅과 분리하여 작성하였습니다.
    
    * records.js #라우터 수정
    
    1. import 경로 수정
    2. 라우터 수정
    
    * recordsController.js #라우터 관련 수정
    
    1. record.js , 라우터 파트에서 recordsController.js 불러오는 부분 수정
    
    * records.js #라우터 수정_2
    
    * recordsController.js #라우터 관련 수정_2
    
    * server.js #record.js 라우터와 연결
    
    * recordsController.js #그룹id, DB 관련 주석처리
    
    develop 브렌치와 머지 진행완료 후 수정
    
    * recordsController.js #수정
    
    * recordsController.js # json 응답 수정

--- src/routes/records.js
+++ src/routes/records.js
@@ -1,5 +1,9 @@
 import express from "express";
+import RecordsController from "../controller/recordsController.js";
 
 const router = express.Router();
+
+// POST - 그룹 운동 기록 생성
+router.post("/:groupId/records", RecordsController.createRecord);
 
 export default router;

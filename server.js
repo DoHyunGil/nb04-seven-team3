0eaed3f988dd4ce41dc4462809312275b13643a8
jhhong0571 <105486482+jhhong0571@users.noreply.github.com>
2025-08-21 10:02:47 +0900

    server.js #likes 라우터 관련

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import groupRouters from "./src/routes/groups.js";
+import likesRouter from "./src/routes/likes.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 app.use("/groups", groupRouters);
+app.use("/groups", likesRouter);
 
 app.listen(PORT, () => {
   console.log("server running");

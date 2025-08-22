064449729f87ac2656375f4b9d7a1097b2fb52a4
Byun JaeYun <bjy001@naver.com>
2025-08-22 16:36:32 +0900

    merge: 그룹 TAG조회 API (#45)
    * groupsController 수정
    
    * tags new upload
    
    * request.http 삭제:wq!
    
    * second update
    
    * :파일 삭제

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import groupRouters from "./src/routes/groups.js";
 import recordsRouter from "./src/routes/records.js";
+import tagsRouters from "./src/routes/tags.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -8,7 +9,8 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 app.use("/groups", groupRouters);
-app.use("/groups", recordsRouter);
+app.use("/records", recordsRouter);
+app.use("/tags", tagsRouters);
 
 app.listen(PORT, () => {
   console.log("server running");

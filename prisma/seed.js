42c46858053c354c0b2147db5bb6b210c5395328
DoHyun <kyu7085@gmail.com>
2025-08-25 15:07:53 +0900

    feat: seed 수정

--- prisma/seed.js
+++ prisma/seed.js
@@ -58,10 +58,38 @@ async function main() {
     // 기존 데이터 삭제
   await prisma.group.deleteMany();
 
-  // 목 데이터 삽입
-  await prisma.group.createMany({
-    data: GROUP,
-    skipDuplicates: true,
+  const user = await prisma.participant.create({
+    data: {
+      nickname: "TEST",
+      password: "PASSWD",
+      group: {
+        connect: { id: group.id },
+      },
+    },
+  });
+
+  prisma.group.update({
+    where: { id: group.id },
+    data: {
+      participant: {
+        connect: { id: user.id },
+      },
+    },
+  });
+
+  const record = await prisma.record.create({
+    data: {
+      type: "RUN",
+      description: "RECORD_DESC",
+      duration: 111,
+      distance: 1123.123123,
+      author: {
+        connect: { id: user.id },
+      },
+      group: {
+        connect: { id: group.id },
+      },
+    },
   });
   */
 }

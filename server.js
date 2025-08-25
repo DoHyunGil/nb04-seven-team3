bad6a0d4498ea23ef4b86b7045154b282aa1c1b3
JaeYun Byun <bjy001@naver.com>
2025-08-25 14:19:44 +0900

    zod add

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import groupRouters from "./src/routes/groups.js";
 import recordsRouter from "./src/routes/records.js";
+import imageRouter from "./src/routes/images.js";
+import path from "path";
 import tagsRouters from "./src/routes/tags.js";
 
 const app = express();
@@ -8,8 +10,10 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
+app.use("/images", express.static(path.join(process.cwd(), "uploads")));
+app.use("/images", imageRouter);
+
 app.use("/groups", groupRouters);
-app.use("/records", recordsRouter);
 app.use("/tags", tagsRouters);
 
 app.listen(PORT, () => {

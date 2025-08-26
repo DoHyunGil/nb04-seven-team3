e2f4127892d21c615fde21d54599a01ba835cc66
juno <juno@jun-oui-noteubug.local>
2025-08-26 18:24:07 +0900

    modified rank

--- src/controller/rankController.js
+++ src/controller/rankController.js
@@ -15,14 +15,14 @@ class rankController {
         }
         else if(rangeType === "WEEKLY"){
             const dayOfWeek = today.getDay()
-            const start = new Date(today); // monday
+            const end = new Date(today); // monday
 
-            start.setDate(today.getDate() -  (dayOfWeek === 0 ? 6: dayOfWeek - 1)) // last of week(sunday)
-            start.setHours(0,0,0,0)
+            end.setDate(today.getDate() -  (dayOfWeek === 0 ? 6: dayOfWeek - 1)) // last of week(sunday)
+            end.setHours(0,0,0,0)
 
-            const end = new Date(start)
-            end.setDate(start.getDate() + 6)
-            end.setHours(23, 59, 59,999)
+            const start = new Date(end)
+            start.setDate(start.getDate() - 6)
+            start.setHours(23, 59, 59,999)
             return {start, end }
         }
         throw new Error("invalid rangeType")
@@ -30,17 +30,19 @@ class rankController {
     // 범위 함수를 매개변수로 받는 비동기 함수 
     async fetchRank(groupId, rangeType){
         const{start, end} = await this.getRange(rangeType)
+        console.log(rangeType);
         console.log(this)
         //랭크 데이터 모델에서 데이터 정렬
         const ranking = await prisma.participant.groupBy({
-            by:['participantId'],
+            by:['id'],
             _count: { id: true },
             where:{
                 ...(groupId ?{groupId : Number(groupId)}: {}),
-                createdAt:{gte :start, lte: end},
+                //createdAt:{gte :start, lte: end},
             },
             orderBy:{_count:{id:"desc"}}
         })
+        console.log("ranking:",ranking)
         return ranking.map((g, idx) => ({
             rank :idx + 1,
             participantId: g.participantId,
@@ -54,7 +56,7 @@ class rankController {
         try {
             const {groupId} = req.params;
             const duration = req.query.duration || "WEEKLY";
-            
+            console.log(duration)
            
     
             if(!groupId || isNaN(groupId)) return res.status(400).json("check groupId")
@@ -63,14 +65,16 @@ class rankController {
             const WEEKLY = await this.fetchRank(Number(groupId), "WEEKLY")
             const MONTHLY = await this.fetchRank(Number(groupId),  "MONTHLY")
 
-            const orderBy = duration === "WEEKLY"? WEEKLY:MONTHLY
+           const orderBy = duration === "WEEKLY"
+            ? { weeklyCount: "desc" }
+            : { monthlyCount: "desc" };
             const rankList = await prisma.group.findMany({
                 where:{ id: groupId},
-                orderBy : {orderBy}
+                orderBy
             })
             console.log(rankList)
             res.status(200).json(rankList)
-        } catch (error) {res.status(200).json({ WEEKLY, MONTHLY })
+        } catch (error) {
               res.status(500).json({ error: error.message });
         }
 
